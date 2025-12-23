62bbf95463889146b3becdf9d3290512e9a12fcb
Krisztián S. <sutyakkrisztian999@hotmail.com>
2025-12-23 03:28:06 +0100

    push

--- packages/@cfx-webview/src/index.ts
+++ packages/@cfx-webview/src/index.ts
@@ -1,2 +1,12 @@
 export * from './EventBridge';
 export * from './Localization';
+
+let resourceName: string = 'UNKNOWN_RESOURCE';
+
+// @ts-ignore
+if (typeof GetParentResourceName === 'function') {
+	// @ts-ignore
+	resourceName = GetParentResourceName();
+}
+
+export { resourceName };
